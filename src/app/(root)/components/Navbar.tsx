2852d42981e78f997b3c0252d2a2a79b7c63eed9
TanaAlvier19 <tanaalvier@gmail.com>
2025-07-01 13:25:21 +0100

    Update Navbar.tsx

--- src/app/(root)/components/Navbar.tsx
+++ src/app/(root)/components/Navbar.tsx
@@ -12,11 +12,7 @@ const Navbar = () => {
     </div>
     {/* ICONS AND USER */}
     <div className='flex items-center gap-6 justify-end w-full'>
-      <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer'>
-      </div>
-      <div className='bg-white rounded-full w-7 h-7 flex items-center justify-center cursor-pointer relative'>
-        <div className='absolute -top-3 -right-3 w-5 h-5 flex items-center justify-center bg-purple-500 text-white rounded-full text-xs'>1</div>
-      </div>
+     
       <div className='flex flex-col'>
         <span className="text-xs leading-3 font-medium"></span>
         <span className="text-[10px] text-gray-500 text-right">Admin</span>
