81a4f352cd2876908b8c2951b8363061f08358f8
TanaAlvier19 <tanaalvier@gmail.com>
2025-07-02 20:32:16 +0100

    ola

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -7,6 +7,11 @@ const config: Config = {
     "./src/components/**/*.{js,ts,jsx,tsx,mdx}",
     "./src/app/**/*.{js,ts,jsx,tsx,mdx}",
   ],
+    safelist: [
+    'w-[20%]',
+    'w-[14%]',
+    'w-[8%]',
+  ],
   theme: {
   	extend: {
   		backgroundImage: {
